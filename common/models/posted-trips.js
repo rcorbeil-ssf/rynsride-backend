c5e8078213814b2305097ed0f3de7fed42a5128e
ssfmaster <ssfmaster@softstackfactory.org>
2016-04-12 20:48:01 +0000

    PostTrip and RequestRide data shows up on Driver/Rider pages

--- common/models/posted-trips.js
+++ common/models/posted-trips.js
@@ -1,7 +1,7 @@
 var locationAllowed = require('../../custom_api/wizardActivity/locationAllowed.js');
-var getNamesApi = require('../../custom_api/postedTrips.js');
+var getNames = require('../../custom_api/postedTrips.js');
 
 module.exports = function(PostedTrips) {
     locationAllowed(PostedTrips);
-    getNamesApi(PostedTrips);
+    getNames(PostedTrips);
 };
