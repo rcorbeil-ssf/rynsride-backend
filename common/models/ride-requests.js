c7fce3adc1f0ee3c96d6592a9a7754d1ffbdcea3
rcorbeil <rcorbeil@softstackfactory.com>
2016-04-16 00:45:28 +0000

    RC: add rideRequest matching

--- common/models/ride-requests.js
+++ common/models/ride-requests.js
@@ -1,6 +1,8 @@
 var historyRiderApi= require('../../custom_api/rideRequests/history-rider.js');
+var computeMatches = require('../../custom_api/matches/compute-matches-requestRide.js');
+
 module.exports = function(RideRequests) {
 
     //historyRiderApi(RideRequests);
-
+    computeMatches(RideRequests);
 };
